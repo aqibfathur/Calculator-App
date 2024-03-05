501ebccf8f2911beabca76752727662bccab0897
aqibfathur <aqibfathurrahman@students.amikom.ac.id>
2024-03-05 12:13:53 +0700

    perubahan baru 2

--- calculator.cs
+++ calculator.cs
@@ -5,8 +5,8 @@ class calculator
 		int a = 10;
 		int b = 6;
 		
-		console.WriteLine("Hasil penambahan : {0} + {1}", a, b, penambahan(a, b));
-		console.WriteLine("Hasil penambahan : {0} - {1}", a, b, pengurangan(a, b ));
+		console.WriteLine("Hasil penambahan : {0} + {1} = {2}", a, b, penambahan(a, b));
+		console.WriteLine("Hasil penambahan : {0} - {1} = {2}", a, b, pengurangan(a, b ));
 		
 		console.WriteLine("\nTekan sembarang key untuk keluar");
 		console.ReadKey();
