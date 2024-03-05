93c6d706f3aeb86e7c6d3fc3a873b7d61f559ec0
aqibfathur <aqibfathurrahman@students.amikom.ac.id>
2024-03-05 12:12:42 +0700

    tugas 4

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,24 @@ class calculator
 {
 	static void main (string[] args)
 	{
-		return a + b;
+		int a = 10;
+		int b = 6;
+		
+		console.WriteLine("Hasil penambahan : {0} + {1}", a, b, penambahan(a, b));
+		console.WriteLine("Hasil penambahan : {0} - {1}", a, b, pengurangan(a, b ));
+		
+		console.WriteLine("\nTekan sembarang key untuk keluar");
+		console.ReadKey();
+	}
+	
+	static int Penambahan( int a,int b )
+	{
+		return a+b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a-b;
 	}
 }
 
